02ba53e98b335e17ab8ee3e60867cf3da311c9ae
raonircl <raoni.afi@gmail.com>
2023-08-09 03:46:46 -0300

    fix: method create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,10 +12,10 @@ class ArticlesController < ApplicationController
   end
 
   def create 
-    @article = Article.new(title: "...", boby: "....")
+    @article = Article.new(article_params)
 
     if @article.save
-      redirect_to @article
+      redirect_to @article, notice: "Create new article!"
     else
       render :new, status: :unprocessable_entity
     end
