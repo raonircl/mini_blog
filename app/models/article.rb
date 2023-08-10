fd124f1bd1b44212055c682adf627eccbebdd6ce
raonircl <raoni.afi@gmail.com>
2023-08-10 13:17:11 -0300

    perf: include module visible inmodels

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,14 +1,8 @@
 class Article < ApplicationRecord
+  include Visible
+
   has_many :comments
 
   validates :title, presence: true, length: { maximum: 15, message: "Exceeded the 15 character limit!" }
   validates :boby, presence: true, length: { minimum: 10, message: "Minimum text with 10 character." }
-
-  VALID_STATUSES = [ 'public', 'private', 'archived' ]
-
-  validates :status, inclusion: { in: VALID_STATUSES }
-
-  def archived?
-    status == 'archived'
-  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,14 +1,8 @@
 class Comment < ApplicationRecord
+  include Visible
+
   belongs_to :article
 
   validates :commenter, presence: true, length: { maximum: 15, message: "Exceeded the 15 character limit!" }
   validates :body, presence: true, length: { maximum: 30, message: "Exceeded the 30 character limit!" }
-
-  VALID_STATUSES = [ 'public', 'private', 'archived' ]
-
-  validates :status, inclusion: { in: VALID_STATUSES }
-
-  def archived?
-    status == 'archived'
-  end
 end
