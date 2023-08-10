6a3a9ab7de6a2017737321088425f7c2174c3c3d
raonircl <raoni.afi@gmail.com>
2023-08-10 02:57:53 -0300

    feat: maximum characters, validates in model comment

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
   has_many :comments
 
-  validates :title, presence: true
-  validates :boby, presence: true, length: { minimum: 10 }
+  validates :title, presence: true, length: { maximum: 15, message: "Exceeded the 15 character limit!" }
+  validates :boby, presence: true, length: { minimum: 10, message: "Minimum text with 10 character." }
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :article
+
+  validates :commenter, presence: true, length: { maximum: 15, message: "Exceeded the 15 character limit!" }
+  validates :body, presence: true, length: { maximum: 30, message: "Exceeded the 30 character limit!" }
 end
