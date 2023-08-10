c948ff874bf8b0bd1688206e21468e8e71048550
raonircl <raoni.afi@gmail.com>
2023-08-10 20:38:20 -0300

    feat: deleting associated objects

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
   include Visible
 
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 15, message: "Exceeded the 15 character limit!" }
   validates :boby, presence: true, length: { minimum: 10, message: "Minimum text with 10 character." }
