88f5edfeb8bfe30dab68775678569e92bc00cbfe
raonircl <raoni.afi@gmail.com>
2023-08-10 20:45:30 -0300

    feat: authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+  http_basic_autheticate_with name: "dhh", password: "secret", except: [ :index, :show ]
+
   def index
     @articles = Article.all
   end
